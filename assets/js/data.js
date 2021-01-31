c7758aecdb83621ae536c7a1ad91fb8be259305f
Gounane Hanane <gounane.hanane@gmail.com>
2021-01-31 16:25:17 +0100

    display pictures

--- assets/js/data.js
+++ assets/js/data.js
@@ -10,15 +10,13 @@ $(document).ready(function() {
            
            var count=0
             data.forEach((item, index) => {
-                if(count==6)
-                    return
-              $(".services").append('<div class="col-lg-4 col-md-6"><div class="single_place"><div class="thumb"><img src="../img/place/1.png" alt=""> </div> <div class="place_info"> <a><h3 onclick="displayService('+item[0]+')">'+item[1]+'</h3></a><p>'+item[2]+'</p><div class="rating_days d-flex justify-content-between"><div class="days"><a href="#">Lire la suite</a></div> </div></div> </div></div>')
-                count++
-               
+              
+              $(".services").append('<div class="col-lg-4 col-md-6"><div class="single_place"><div class="thumb"><img width="350" height="197" src="'+window.location.origin+'/services/'+item[4]+'" alt=""> </div> <div class="place_info"> <a><h3 onclick="displayService('+item[0]+')">'+item[1]+'</h3></a><p>'+item[2]+'</p><div class="rating_days d-flex justify-content-between"><div class="days"><a href="#">Lire la suite</a></div> </div></div> </div></div>')
+           
               })
               data.forEach((item, index) => {
               $("#menu_service").append('<li><a href="'+window.location.origin+"/services/front-end/service.php?"+item[0]+'"> '+item[1]+'</a></li>')
-                count++
+              
                
               })
               
